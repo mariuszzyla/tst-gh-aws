7ec5205fd1ae3830f6d2b9095f7389501462d63c
MrVein <mariusz.zyla@gmail.com>
2023-05-07 18:11:36 +0200

    Initial commit

---
+++ iam.tf
@@ -0,0 +1,34 @@
+resource "aws_iam_openid_connect_provider" "default" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+data "aws_iam_policy_document" "instance_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = ["${aws_iam_openid_connect_provider.default.arn}"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:aud"
+      values = [var.OIDCAudience]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = ["repo:${var.GitHubOrg}/${var.RepositoryName}:*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "gh-role" {
+  name               = "gh-iam-role"
+  assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
+}
---
+++ vars.tf
@@ -0,0 +1,14 @@
+variable "OIDCAudience" {
+    type = string
+    default = "sts.amazonaws.com"
+}
+
+variable "GitHubOrg" {
+    type = string
+    default = "mariuszzyla"
+}
+
+variable "RepositoryName" {
+    type = string
+    default = "tst-gh-aws"
+}
